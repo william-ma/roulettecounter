7724dadc0466c4964c5333880774eb16d6567d90
William Ma <willma028@gmail.com>
2020-01-23 22:47:03 +1100

    Implemented counting functionality

--- roulettecounter/views.py
+++ roulettecounter/views.py
@@ -5,31 +5,36 @@ import datetime
 # Create your views here.
 def homepage(request):
     context = {}
+    context['sessionMessage'] = ""
+    currentSession = getCurrentSession()
 
-    currentSession = None
     if request.method == "POST":
         if request.POST.get('start_session', False):
             if not isInASession():
                 currentSession = startSession()
+                context['sessionMessage'] = "Session started at " + str(currentSession.dateStart)
+            else:
+                context['sessionMessage'] = "Already in a session started on " + str(currentSession.dateStart)
         elif request.POST.get('finish_session', False):
             if isInASession():
-                finishSession()
-    else:
-        # should i programmatically... generate the html? i suppose i do need to pass the "count" to that website to actually show the count... otherwise it's kind of useless...
-        # so i'll need to pass all 37 numbers into the field into context.
-        print("method: " + request.method)
-        # hold a variable... whether i'm in a session or not...
-        # pass that variable onwards?
-        # go to db... get the last session and see if the date has ended...
-        try:
-            currentSession = Session.objects.latest('dateStart')
-        except Session.DoesNotExist:
-            pass
-            # inSession = currentSession.dateEnd != None
-            # if inSession:
-            #     context['inSession'] = inSession
+                finishedSession = finishSession()
+                context['sessionMessage'] = "Session ended on " + str(finishedSession.dateEnd)
+            else:
+                context['sessionMessage'] = "No session to finish"
+        elif request.POST.get('number', False):
+            if isInASession():
+                number = request.POST.get('number', False)
+                numberObj = Number.objects.get(number=number, session=currentSession)
+                numberObj.count += 1
+                numberObj.save()
+            else:
+                context['sessionMessage'] = "Must be in a session to use numbers"
+    elif request.method == "GET":
+        if isInASession():
+            context['sessionMessage'] = "Currently in a session started on " + str(currentSession.dateStart)
 
     context['currentSession'] = currentSession
+    context['numbers'] = Number.objects.filter(session=currentSession)
 
     return render(request=request, template_name="roulettecounter/home.html", context=context)
 
@@ -64,7 +69,10 @@ def startSession():
     return session
 
 def finishSession():
+    print("finishSession()")
     session = getCurrentSession()
     if session:
         session.dateEnd = datetime.datetime.now()
         session.save()
+
+    return session
