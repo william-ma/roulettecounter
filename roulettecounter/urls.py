3910202c7646b319a21da05bad31d7993e3927e7
William Ma <willma028@gmail.com>
2020-02-21 16:59:44 +1100

    Remove analytics page, already in homepage

--- roulettecounter/urls.py
+++ roulettecounter/urls.py
@@ -34,8 +34,6 @@ urlpatterns = [
     path('number/<int:number>', views.number_request, name="number"),
     path('delete_most_recent', views.delete_most_recent_request, name="delete_most_recent"),
     path('history', views.history_request, name="history"),
-    path('analytics', views.analytics_request, name="analytics"),
     path('mobile', views.mobile_request, name="mobile"),
     path('', include(router.urls))
-    #path('contact/', include('contact_form.urls')),
 ]
